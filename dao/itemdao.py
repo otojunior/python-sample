b8ac38ae20381b3b4d2fcdcbe4b39997dab02530
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2019-01-22 13:42:00 -0200

    Criação da tabela de item automaticamente, caso não exista

--- dao/itemdao.py
+++ dao/itemdao.py
@@ -8,15 +8,6 @@ import sqlite3
 from dao.basedao import BaseDao
 
 class ItemDao (BaseDao):
-    def create_table(self):
-        sql = """create table item (
-            id integer primary key,
-            codigo integer not null,
-            nome varchar(30) not null)"""
-        cursor = self.conn.cursor()
-        cursor.execute(sql)
-        cursor.close()
-        
     def insert(self, row):
         sql = "insert into item values (?, ?, ?)"
         cursor = self.conn.cursor()
@@ -25,14 +16,29 @@ class ItemDao (BaseDao):
         self.conn.commit()        
     
     def find_all(self):
-        sql = "select * from item"
+        try:
+            sql = "select * from item"
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+            cursor.close()
+            return rows
+        except sqlite3.OperationalError:
+            self.__create_table()
+            self.__insert_some_data()
+            print("[DEBUG] Create table ITEM and inserted some data")
+            return self.find_all()
+
+    def __create_table(self):
+        sql = """create table item (
+            id integer primary key,
+            codigo integer not null,
+            nome varchar(30) not null)"""
         cursor = self.conn.cursor()
         cursor.execute(sql)
-        rows = cursor.fetchall()
         cursor.close()
-        return rows
-    
-    def __test_insertsomedata(self):
-        for i in range(1,51):
+
+    def __insert_some_data(self):
+        for i in range(1,6):
             row = (i, i*100, "Produto%d"%i)
             self.insert(row)    
